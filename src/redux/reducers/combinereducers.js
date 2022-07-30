0c2d7244a23db86d76d7a0d2d84c5f07ee500c14
juhishivam <juhikumari5844@gmail.com>
2022-07-30 20:46:32 +0530

    changes

--- src/redux/reducers/combinereducers.js
+++
@@ -1,9 +0,0 @@
-import Reducer from "./contactreducer";
-import { combineReducers } from "redux";
-
-
-const combinereducer =combineReducers({
-Reducer
-})
-
-export default combinereducer;
--- src/redux/type.js
+++
@@ -1,1 +0,0 @@
-export const POST_DETAILS = "POST_DETAILS"
