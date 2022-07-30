bbcbe1ca169bbe7839a695dd1dd6e0e16f4d6442
juhishivam <juhikumari5844@gmail.com>
2022-07-30 20:48:47 +0530

    chnage.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './redux/store';
-import {Provider} from 'react-redux'
+
 
 ReactDOM.render(
   <>
     <BrowserRouter>
-    <Provider store={store}>
+    
       <App />
-      </Provider>
+      
     </BrowserRouter>
   </>,
   document.getElementById('root')
